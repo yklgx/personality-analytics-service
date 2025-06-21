5e79d5ebb034dbf8fc581643ca23c4b577f12a38
ItsMaryl <mary.lampropoulou1@gmail.com>
2025-06-21 22:29:46 +0300

    Add files via upload

--- ml_pipeline/analysis.py
+++ ml_pipeline/analysis.py
@@ -1,27 +1,27 @@
 import pandas as pd
 
-# Φόρτωση δεδομένων από τον φάκελο "data"
+# Import data
 df = pd.read_csv("ml_pipeline/personality_dataset.csv")
 
-# Πρώτες γραμμές
+# first lines
 print(" Πρώτες γραμμές:")
 print(df.head())
 
-# Πληροφορίες για τύπους δεδομένων
+# Info for data type
 print("\n Πληροφορίες:")
 print(df.info())
 
-# Πόσες κενές τιμές έχει κάθε στήλη
+# Drop rows with missing values
 print("\n Κενές τιμές:")
 print(df.isnull().sum())
 
 df = df.dropna()
 
-# Μετατροπή των "Yes/No" σε 1/0
+# Convert "Yes/No" to 1/0
 df["Stage_fear"] = df["Stage_fear"].map({"Yes": 1, "No": 0})
 df["Drained_after_socializing"] = df["Drained_after_socializing"].map({"Yes": 1, "No": 0})
 
-# Μετατροπή του "Personality" σε 0 (Introvert) και 1 (Extrovert)
+# Convert "Personality" to 0 (Introvert) and 1 (Extrovert)
 df["Personality"] = df["Personality"].map({"Introvert": 0, "Extrovert": 1})
 
 print("\n Μοναδικές τιμές στο Personality:")
@@ -34,21 +34,21 @@ from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import accuracy_score, classification_report, confusion_matrix
 
-# Χωρισμός χαρακτηριστικών (X) και στόχου (y)
+# Split features (X) and target (y)
 X = df.drop("Personality", axis=1)
 y = df["Personality"]
 
-# Διαχωρισμός σε training και testing sets (80% / 20%)
+# Split to training and testing sets (80% / 20%)
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
-# Δημιουργία και εκπαίδευση μοντέλου
+# Create and train model
 model = LogisticRegression(max_iter=1000)
 model.fit(X_train, y_train)
 
-# Πρόβλεψη στο test set
+# Prediction of test set
 y_pred = model.predict(X_test)
 
-# Αξιολόγηση μοντέλου
+# Evaluate model
 print("\n Ακρίβεια (accuracy):", accuracy_score(y_test, y_pred))
 print("\n Confusion matrix:\n", confusion_matrix(y_test, y_pred))
 print("\n Classification report:\n", classification_report(y_test, y_pred))
